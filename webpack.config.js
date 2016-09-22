9616488753513a1db91fc2d598fabacd3a37cca0
Samer Buna <samer.buna@gmail.com>
2016-09-22 13:44:18 -0700

    setup

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
