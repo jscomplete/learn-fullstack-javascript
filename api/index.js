6bdb1540889bdf6d4e154ff2eaf58e7c6d324d13
Samer Buna <samer.buna@gmail.com>
2016-10-12 15:51:25 -0700

    4.5-end

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import data from '../src/testData';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send({ data: [] });
+router.get('/contests', (req, res) => {
+  res.send({ contests: data.contests });
 });
 
 export default router;
