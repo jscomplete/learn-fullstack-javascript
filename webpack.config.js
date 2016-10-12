25f4f13724b24d0b6e66ba05d719cf4bc94981d7
Samer Buna <samer.buna@gmail.com>
2016-10-12 14:33:57 -0700

    4.1-end

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
   module: {
     loaders: [
       {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
         test: /\.js$/,
         loader: 'babel-loader'
       }
