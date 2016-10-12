00b2592958ccd73337209cb47f1de3aab3b38859
Samer Buna <samer.buna@gmail.com>
2016-10-12 14:44:25 -0700

    4.2-end

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import ContestPreview from './ContestPreview';
 
 class App extends React.Component {
   state = {
@@ -16,7 +17,9 @@ class App extends React.Component {
       <div className="App">
         <Header message={this.state.pageHeader} />
         <div>
-          ...
+          {this.props.contests.map(contest =>
+            <ContestPreview {...contest} />
+          )}
         </div>
       </div>
     );
