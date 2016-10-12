c490fc3b20a784ac11870ed39c494b1d7530a99d
Samer Buna <samer.buna@gmail.com>
2016-10-12 12:36:50 -0700

    3.3-end

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (props) => {
+const Header = ({ message }) => {
   return (
     <h2 className="text-center">
-      {props.headerMessage}
+      {message}
     </h2>
   );
 };
 
-App.propTypes = {
-  headerMessage: React.PropTypes.string
+Header.propTypes = {
+  message: React.PropTypes.string
 };
 
-App.defaultProps = {
-  headerMessage: 'Hello!!'
+const App = () => {
+  return (
+    <div className="App">
+      <Header message="Naming Contests" />
+      <div>
+        ...
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(
