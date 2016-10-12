939bfc0d08164bb44041c4c0d79a5d7cf8f01dc1
Samer Buna <samer.buna@gmail.com>
2016-10-12 15:33:46 -0700

    4.4-end

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
+import data from '../testData';
 
 class App extends React.Component {
   state = {
-    pageHeader: 'Naming Contests'
+    pageHeader: 'Naming Contests',
+    contests: []
   };
   componentDidMount() {
-    // timers, listeners
+    this.setState({
+      contests: data.contests
+    });
   }
   componentWillUnmount() {
     // clean timers, listeners
@@ -17,8 +21,8 @@ class App extends React.Component {
       <div className="App">
         <Header message={this.state.pageHeader} />
         <div>
-          {this.props.contests.map(contest =>
-            <ContestPreview {...contest} />
+          {this.state.contests.map(contest =>
+            <ContestPreview key={contest.id} {...contest} />
           )}
         </div>
       </div>
