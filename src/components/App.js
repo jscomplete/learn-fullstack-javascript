c5793864f9d84ed6197c13082c8e1b33356f8dc2
Samer Buna <samer.buna@gmail.com>
2016-10-12 15:51:25 -0700

    4.5-end

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
-import data from '../testData';
 
 class App extends React.Component {
   state = {
@@ -9,9 +9,13 @@ class App extends React.Component {
     contests: []
   };
   componentDidMount() {
-    this.setState({
-      contests: data.contests
-    });
+    axios.get('/api/contests')
+      .then(resp => {
+        this.setState({
+          contests: resp.data.contests
+        });
+      })
+      .catch(console.error);
   }
   componentWillUnmount() {
     // clean timers, listeners
