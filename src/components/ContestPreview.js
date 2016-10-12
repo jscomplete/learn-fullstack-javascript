ed154000427dff84ea44e3958d0f9d79bd305269
Samer Buna <samer.buna@gmail.com>
2016-10-12 15:05:01 -0700

    4.3-end

--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const ContestPreview = (contest) => (
   <div className="ContestPreview">
-    <div>
+    <div className="category-name">
       {contest.categoryName}
     </div>
-    <div>
+    <div className="contest-name">
       {contest.contestName}
     </div>
   </div>
