b5160c4597c6b06353b9d66cf46880e419a0dd65
Samer Buna <samer.buna@gmail.com>
2016-10-12 15:05:01 -0700

    4.3-end

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import config from './config';
 import apiRouter from './api';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
 
 import express from 'express';
 const server = express();
+
+server.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public')
+}));
 
 server.set('view engine', 'ejs');
 
