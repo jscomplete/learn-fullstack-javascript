a83d48b5e8c676867e3c5069d3a70e8965012009
Samer Buna <samer.buna@gmail.com>
2016-10-12 15:33:46 -0700

    4.4-end

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import data from './testData';
 import App from './components/App';
 
 ReactDOM.render(
-  <App contests={data.contests} />,
+  <App />,
   document.getElementById('root')
 );
