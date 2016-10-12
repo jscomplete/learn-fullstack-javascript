64b93acf1b7a0b30b8f7366af22ecb483e30e763
Samer Buna <samer.buna@gmail.com>
2016-10-12 10:50:39 -0700

    3.1-end

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h2 className="text-center">
+    Hello React with JSX!!
+  </h2>,
+  document.getElementById('root')
+);
