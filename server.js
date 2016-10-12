d1f7062e4e05b904219de8c4cf239b73b90f3ca4
Samer Buna <samer.buna@gmail.com>
2016-10-11 18:39:06 -0700

    2.4-end

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ import apiRouter from './api';
 import express from 'express';
 const server = express();
 
+server.set('view engine', 'ejs');
+
 server.get('/', (req, res) => {
-  res.send('Hello Express');
+  res.render('index', {
+    content: '...'
+  });
 });
 
 server.use('/api', apiRouter);
