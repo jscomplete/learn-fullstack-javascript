16b16fe74fe0d046cd4dccf01d50d516906a93f9
Samer Buna <samer.buna@gmail.com>
2016-10-12 21:01:06 -0700

    5.1-end

--- config.js
+++ config.js
@@ -9,5 +9,9 @@ export const logStars = function(message) {
 };
 
 export default {
-  port: env.PORT || 8080
+  port: env.PORT || 8080,
+  host: env.HOST || '0.0.0.0',
+  get serverUrl() {
+    return `http://${this.host}:${this.port}`;
+  }
 };
