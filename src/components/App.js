33426330b402a5cd90dcc4217a30f2ce1582836d
Samer Buna <samer.buna@gmail.com>
2016-10-12 21:38:45 -0700

    5.3-end

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
 
@@ -9,13 +8,7 @@ class App extends React.Component {
     contests: this.props.initialContests
   };
   componentDidMount() {
-    axios.get('/api/contests')
-      .then(resp => {
-        this.setState({
-          contests: resp.data.contests
-        });
-      })
-      .catch(console.error);
+
   }
   componentWillUnmount() {
     // clean timers, listeners
