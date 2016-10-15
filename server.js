f0b78384c6b58292cd9262f42e760bfbcebb8eee
Samer Buna <samer.buna@gmail.com>
2016-10-14 19:59:09 -0700

    6.1-begin

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ import config from './config';
 import apiRouter from './api';
 import sassMiddleware from 'node-sass-middleware';
 import path from 'path';
-
+import serverRender from './serverRender';
 import express from 'express';
+
 const server = express();
 
 server.use(sassMiddleware({
@@ -12,8 +13,6 @@ server.use(sassMiddleware({
 }));
 
 server.set('view engine', 'ejs');
-
-import serverRender from './serverRender';
 
 server.get('/', (req, res) => {
   serverRender()
