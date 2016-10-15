edd6a4ae12d913103381ac2208ba986c1ed2a2d5
Samer Buna <samer.buna@gmail.com>
2016-10-15 13:21:28 -0700

    6.8-end

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,24 @@ import * as api from '../api';
 const pushState = (obj, url) =>
   window.history.pushState(obj, '', url);
 
+const onPopState = handler => {
+  window.onpopstate = handler;
+};
+
 class App extends React.Component {
   static propTypes = {
     initialData: React.PropTypes.object.isRequired
   };
   state = this.props.initialData;
   componentDidMount() {
-
+    onPopState((event) => {
+      this.setState({
+        currentContestId: (event.state || {}).currentContestId
+      });
+    });
   }
   componentWillUnmount() {
-    // clean timers, listeners
+    onPopState(null);
   }
   fetchContest = (contestId) => {
     pushState(
@@ -27,7 +35,7 @@ class App extends React.Component {
       this.setState({
         currentContestId: contest.id,
         contests: {
-          ...this.state.cotests,
+          ...this.state.contests,
           [contest.id]: contest
         }
       });
