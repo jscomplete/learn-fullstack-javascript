2e9ee2f5818d00964201ee7715addbce03fad541
Samer Buna <samer.buna@gmail.com>
2016-10-15 11:50:27 -0700

    6.6-end

--- serverRender.js
+++ serverRender.js
@@ -6,14 +6,36 @@ import App from './src/components/App';
 import config from './config';
 import axios from 'axios';
 
-const serverRender = () =>
-  axios.get(`${config.serverUrl}/api/contests`)
+const getApiUrl = contestId => {
+  if (contestId) {
+    return `${config.serverUrl}/api/contests/${contestId}`;
+  }
+  return `${config.serverUrl}/api/contests`;
+};
+
+const getInitialData = (contestId, apiData) => {
+  if (contestId) {
+    return {
+      currentContestId: apiData.id,
+      contests: {
+        [apiData.id]: apiData
+      }
+    };
+  }
+  return {
+    contests: apiData.contests
+  };
+};
+
+const serverRender = (contestId) =>
+  axios.get(getApiUrl(contestId))
     .then(resp => {
+      const initialData = getInitialData(contestId, resp.data);
       return {
         initialMarkup: ReactDOMServer.renderToString(
-          <App initialData={resp.data} />
+          <App initialData={initialData} />
         ),
-        initialData: resp.data
+        initialData
       };
     });
 
