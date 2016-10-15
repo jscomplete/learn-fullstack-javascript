6348c2cd715d1bd4d3fa2521dd5d51c33ff371c9
Samer Buna <samer.buna@gmail.com>
2016-10-15 15:07:58 -0700

    7.1-end

--- api/index.js
+++ api/index.js
@@ -1,23 +1,42 @@
 import express from 'express';
-import data from '../src/testData';
+import { MongoClient } from 'mongodb';
+import assert from 'assert';
+import config from '../config';
+
+let mdb;
+MongoClient.connect(config.mongodbUri, (err, client) => {
+  assert.equal(null, err);
+
+  mdb = client.db('test');
+});
 
 const router = express.Router();
-const contests = data.contests.reduce((obj, contest) => {
-  obj[contest.id] = contest;
-  return obj;
-}, {});
 
 router.get('/contests', (req, res) => {
-  res.send({
-    contests: contests
-  });
+  let contests = {};
+  mdb.collection('contests').find({})
+    .project({
+      id: 1,
+      categoryName: 1,
+      contestName: 1
+    })
+    .each((err, contest) => {
+      assert.equal(null, err);
+
+      if (!contest) { // no more contests
+        res.send({ contests });
+        return;
+      }
+
+      contests[contest.id] = contest;
+    });
 });
 
 router.get('/contests/:contestId', (req, res) => {
-  let contest = contests[req.params.contestId];
-  contest.description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-
-  res.send(contest);
+  mdb.collection('contests')
+    .findOne({ id: Number(req.params.contestId) })
+    .then(contest => res.send(contest))
+    .catch(console.error);
 });
 
 export default router;
