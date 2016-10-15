7e06372a7c6b3397cc36a7e69d5985545824fc87
Samer Buna <samer.buna@gmail.com>
2016-10-15 16:50:54 -0700

    7.3-end

--- src/components/Contest.js
+++ src/components/Contest.js
@@ -1,6 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 
 class Contest extends Component {
+  componentDidMount() {
+    this.props.fetchNames(this.props.nameIds);
+  }
   render() {
     return (
       <div className="Contest">
@@ -21,8 +24,11 @@ class Contest extends Component {
           </div>
           <div className="panel-body">
             <ul className="list-group">
-              <li className="list-group-item">Name one...</li>
-              <li className="list-group-item">Name two...</li>
+              {this.props.nameIds.map(nameId =>
+                <li key={nameId} className="list-group-item">
+                  {this.props.lookupName(nameId).name}
+                </li>
+              )}
             </ul>
           </div>
         </div>
@@ -55,6 +61,9 @@ class Contest extends Component {
 Contest.propTypes = {
   description: PropTypes.string.isRequired,
   contestListClick: PropTypes.func.isRequired,
+  fetchNames: PropTypes.func.isRequired,
+  nameIds: PropTypes.array.isRequired,
+  lookupName: PropTypes.func.isRequired,
 };
 
 export default Contest;
