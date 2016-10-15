bc558760c65fac3bb82dcc540ccb6f3f8b3b635e
Samer Buna <samer.buna@gmail.com>
2016-10-15 15:36:53 -0700

    7.2-begin

--- src/components/Contest.js
+++ src/components/Contest.js
@@ -4,9 +4,45 @@ class Contest extends Component {
   render() {
     return (
       <div className="Contest">
-        <div className="contest-description">
-          {this.props.description}
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">Contest Description</h3>
+          </div>
+          <div className="panel-body">
+            <div className="contest-description">
+              {this.props.description}
+            </div>
+          </div>
         </div>
+
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">Proposed Names</h3>
+          </div>
+          <div className="panel-body">
+            <ul className="list-group">
+              <li className="list-group-item">Name one...</li>
+              <li className="list-group-item">Name two...</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="panel panel-info">
+          <div className="panel-heading">
+            <h3 className="panel-title">Propose a New Name</h3>
+          </div>
+          <div className="panel-body">
+            <form>
+              <div className="input-group">
+                <input type="text" placeholder="New Name Here..." className="form-control" />
+                <span className="input-group-btn">
+                  <button type="submit" className="btn btn-info">Sumbit</button>
+                </span>
+              </div>
+            </form>
+          </div>
+        </div>
+
         <div className="home-link link"
              onClick={this.props.contestListClick}>
           Contest List
