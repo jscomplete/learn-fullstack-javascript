0976a13f928f45ff14f4b2d2cb4898b8308983c6
Samer Buna <samer.buna@gmail.com>
2016-10-15 13:31:01 -0700

    7.1-begin

--- config.js
+++ config.js
@@ -9,6 +9,7 @@ export const logStars = function(message) {
 };
 
 export default {
+  mongodbUri: 'mongodb://localhost:27017/test',
   port: env.PORT || 8080,
   host: env.HOST || '0.0.0.0',
   get serverUrl() {
