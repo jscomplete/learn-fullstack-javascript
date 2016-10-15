28b8436b7f65dc0f12ae3fdbfffc13dccbcc9002
Samer Buna <samer.buna@gmail.com>
2016-10-14 22:22:29 -0700

    6.2-end

--- src/components/ContestList.js
+++ src/components/ContestList.js
@@ -2,16 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ContestPreview from './ContestPreview';
 
-const ContestList = ({ contests }) => (
+const ContestList = ({ contests, onContestClick }) => (
   <div className="ContestList">
     {contests.map(contest =>
-      <ContestPreview key={contest.id} {...contest} />
+      <ContestPreview
+        key={contest.id}
+        onClick={onContestClick}
+        {...contest} />
     )}
   </div>
 );
 
 ContestList.propTypes = {
-  contests: PropTypes.array
+  contests: PropTypes.array,
+  onContestClick: PropTypes.func.isRequired,
 };
 
 export default ContestList;
--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class ContestPreview extends Component {
   handleClick = () => {
-    console.log(this.props.contestName);
+    this.props.onClick(this.props.id);
   };
   render() {
     return (
@@ -20,8 +20,10 @@ class ContestPreview extends Component {
 }
 
 ContestPreview.propTypes = {
+  id: PropTypes.number.isRequired,
   categoryName: PropTypes.string.isRequired,
   contestName: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default ContestPreview;
