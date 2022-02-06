44302fdf489925285a36a9e396405b616109a9ba
Claudia <caucrah@gmail.com>
2022-02-06 17:17:41 +0100

    config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,28 +6,22 @@ module.exports = {
     node: true,
     jest: true,
   },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
-    ecmaVersion: 2020,
     ecmaFeatures: {
-      impliedStrict: true,
       jsx: true,
     },
     sourceType: "module",
   },
-  plugins: ["react", "react-hooks"],
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-  ],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+  plugins: ["react"],
   rules: {
-    // You can do your customizations here...
-    // For example, if you don't want to use the prop-types package,
-    // you can turn off that recommended rule with: 'react/prop-types': ['off']
+    indent: ["error", 2, { SwitchCase: 1 }],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-console": [
+      "warn",
+      { allow: ["clear", "info", "error", "dir", "trace"] },
+    ],
   },
 };
