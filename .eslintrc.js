5fb2c1896ed41f3f2a839d10f6c1a5f762519f14
Claudia <caucrah@gmail.com>
2022-02-06 16:40:56 +0100

    config

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true,
+    },
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    // You can do your customizations here...
+    // For example, if you don't want to use the prop-types package,
+    // you can turn off that recommended rule with: 'react/prop-types': ['off']
+  },
+};
---
+++ babel.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  presets: ["@babel/react", "@babel/preset-env"],
+  plugins: ["@babel/plugin-proposal-class-properties"],
+};
